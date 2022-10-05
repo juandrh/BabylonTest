881e02e7ed5a8a92135992a20ff1e88ae3fca779
Juan Del Rio <52029674+juandrh@users.noreply.github.com>
2022-10-05 19:17:07 +0200

    Add files via upload

---
+++ agent.js
@@ -0,0 +1,89 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// Evolution EcoSystem
+
+// Creature class
+
+// Create a "bloop" creature
+
+
+
+class Agent {
+  constructor() {
+
+    this.r =Math.random()*3;
+    this.maxspeed = (3-this.r)/1;
+
+    this.agentMesh = BABYLON.MeshBuilder.CreateSphere(
+      "sphere",
+      { diameter: 2*this.r, segments: 8 },
+      scene
+    );
+
+    this.agentMesh.position.x= Math.random()*120-60;
+    this.agentMesh.position.z= Math.random()*120-60;
+    this.health = 200; // Life timer
+    this.xoff = Math.random()*1000; // For perlin noise
+    this.zoff = Math.random()*1000; 
+   
+     
+  }
+
+  run() {
+    this.update();
+    this.borders();
+    //this.display();
+  }
+
+ 
+
+  // At any moment there is a teeny, tiny chance a bloop will reproduce
+ /*  reproduce() {
+    // asexual reproduction
+    if (random(1) < 0.0005) {
+      // Child is exact copy of single parent
+      let childDNA = this.dna.copy();
+      // Child DNA can mutate
+      childDNA.mutate(0.01);
+      return new Bloop(this.position, childDNA);
+    } else {
+      return null;
+    }
+  } */
+
+  
+  update() {
+    
+    let vx = (noise.perlin2(this.xoff,0))*this.maxspeed; 
+    let vz = (noise.perlin2(0,this.zoff))*this.maxspeed; 
+    this.xoff += 0.005;
+    this.zoff += 0.005;   
+    this.agentMesh.position.x += vx;
+    this.agentMesh.position.z += vz;
+    
+    this.health -= 0.2;
+  }
+
+ 
+  borders() {
+    const width = 150;
+    if (this.agentMesh.position.x < -width/2+this.r/2) this.agentMesh.position.x = width/2-this.r/2;
+    if (this.agentMesh.position.x > width/2-this.r/2) this.agentMesh.position.x = -width/2+this.r/2;
+    if (this.agentMesh.position.z < -width/2+this.r/2) this.agentMesh.position.z = width/2-this.r/2;    
+    if (this.agentMesh.position.z > width/2-this.r/2) this.agentMesh.position.z = -width/2+this.r/2;
+  }
+
+  isDead() {
+    if (this.health < 0.0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  getRadius(){
+  return this.r;
+  }
+}
---
+++ food.js
@@ -0,0 +1,63 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// Evolution EcoSystem
+
+// A collection of food in the world
+
+class Food {
+
+  
+  constructor(num) {
+    // Start with some food
+    var myMaterial = new BABYLON.StandardMaterial("myMaterial", scene);
+      myMaterial.diffuseColor = new BABYLON.Color3(1, 0, 1);
+      myMaterial.specularColor = new BABYLON.Color3(0.5, 0.6, 0.87);
+      myMaterial.emissiveColor = new BABYLON.Color3(0.0, 0.58, 0.02);
+      myMaterial.ambientColor = new BABYLON.Color3(0.0, 0.58, 0.02);
+    this.food = [];
+    for (let i = 0; i < num; i++) {
+      this.foodMesh = BABYLON.Mesh.CreateBox("box", 2,scene);
+      this.foodMesh.position.x= Math.random()*120-60;
+      this.foodMesh.position.z= Math.random()*120-60;
+      this.foodMesh.material=myMaterial ;
+     this.food.push(this.foodMesh); 
+    }
+  }
+
+   // Add new food item
+   addFood() {
+    var myMaterial = new BABYLON.StandardMaterial("myMaterial", scene);
+
+      myMaterial.diffuseColor = new BABYLON.Color3(1, 0, 1);
+      myMaterial.specularColor = new BABYLON.Color3(0.5, 0.6, 0.87);
+      myMaterial.emissiveColor = new BABYLON.Color3(0.0, 0.58, 0.02);
+      myMaterial.ambientColor = new BABYLON.Color3(0.0, 0.58, 0.02);
+    this.foodMesh =  BABYLON.Mesh.CreateBox("box", 2,scene);
+    this.foodMesh.position.x= Math.random()*120-60;
+    this.foodMesh.position.z= Math.random()*120-60;
+    this.foodMesh.material=myMaterial ;
+   this.food.push(this.foodMesh);    
+    
+  } 
+
+  eatFood(i){
+    this.food[i].dispose();
+    this.food.splice(i,1);
+    this.addFood();
+  }
+  
+  // Return the list of food
+  getFood() {
+    return this.food;
+  } 
+
+  getFoodLength(){
+    return this.food.length;
+  }
+
+  getFoodPosition(i){
+    return new BABYLON.Vector2(this.food[i].position.x,this.food[i].position.z);
+  }
+}
